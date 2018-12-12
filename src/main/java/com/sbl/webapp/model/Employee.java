a8ff1f8f4149ec6ff18997bf5bc18a65845ed3cb
LAPTOP-452QGQ9Q\rvc41 <venkat90001@gmail.com>
2018-12-11 22:42:22 -0800

    Employee model File is added

---
+++ src/main/java/com/sbl/webapp/model/Employee.java
@@ -0,0 +1,130 @@
+package com.sbl.webapp.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name="Employee")
+@EntityListeners(AuditingEntityListener.class)
+
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@NotBlank
+	private String firstName;
+	
+	@NotBlank
+	private String lastName;
+	
+	@NotBlank
+	private String email;
+	
+	@NotBlank
+	private Long salary;
+	
+	@NotBlank
+	private String designation;
+	
+	@NotBlank
+	private String team;
+	
+	@NotBlank
+	private Date crDt;	
+	
+	@NotBlank
+	private String profile;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public Date getCrDt() {
+		return crDt;
+	}
+
+	public void setCrDt(Date crDt) {
+		this.crDt = crDt;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", salary=" + salary + ", designation=" + designation + ", team=" + team + ", crDt=" + crDt
+				+ ", profile=" + profile + "]";
+	}
+	
+	
+	
+}
